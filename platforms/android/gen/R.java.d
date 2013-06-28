dc2c253bad9b3a8ec4db05f8e8dd89e5e89f8356
adamleystrnad <adamleystrnad@gmail.com>
2013-06-28 15:59:55 -0400

    added android

---
+++ platforms/android/gen/R.java.d
@@ -0,0 +1,14 @@
+/Users/ThoughtWorks/Projects/Arrive/platforms/android/gen/io/cordova/hellocordova/R.java \
+ : /Users/ThoughtWorks/Projects/Arrive/platforms/android/res/drawable/icon.png \
+/Users/ThoughtWorks/Projects/Arrive/platforms/android/res/drawable-hdpi/ic_launcher.png \
+/Users/ThoughtWorks/Projects/Arrive/platforms/android/res/drawable-hdpi/icon.png \
+/Users/ThoughtWorks/Projects/Arrive/platforms/android/res/drawable-ldpi/ic_launcher.png \
+/Users/ThoughtWorks/Projects/Arrive/platforms/android/res/drawable-ldpi/icon.png \
+/Users/ThoughtWorks/Projects/Arrive/platforms/android/res/drawable-mdpi/ic_launcher.png \
+/Users/ThoughtWorks/Projects/Arrive/platforms/android/res/drawable-mdpi/icon.png \
+/Users/ThoughtWorks/Projects/Arrive/platforms/android/res/drawable-xhdpi/ic_launcher.png \
+/Users/ThoughtWorks/Projects/Arrive/platforms/android/res/drawable-xhdpi/icon.png \
+/Users/ThoughtWorks/Projects/Arrive/platforms/android/res/layout/main.xml \
+/Users/ThoughtWorks/Projects/Arrive/platforms/android/res/values/strings.xml \
+/Users/ThoughtWorks/Projects/Arrive/platforms/android/res/xml/config.xml \
+/Users/ThoughtWorks/Projects/Arrive/platforms/android/bin/AndroidManifest.xml \
